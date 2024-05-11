cf3ea419c76486ced03eb8d3c6025416835ec71e
Nenad Jakovchevski <89221061@student.upr.si>
2024-05-11 21:18:00 +0200

    FIX LOGIN YOOOO

--- frontend/src/Components/Login.js
+++ frontend/src/Components/Login.js
@@ -1,12 +1,13 @@
 import React, { useState } from 'react';
 import axios from 'axios';
-import Navbar from './Navbar';  // Correctly imported as you're using it in this component
+import Navbar from './Navbar';
+import './css/login.css'; // Ensure the CSS file is correctly referenced
 
 const Login = () => {
     const [username, setUsername] = useState('');
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
-    const [walletAddress, setWalletAddress] = useState('');  // New state for wallet address
+    const [walletAddress, setWalletAddress] = useState('');
 
     const handleSubmit = async (e) => {
         e.preventDefault();
@@ -14,7 +15,6 @@ const Login = () => {
             const response = await axios.post('/api/login', { username, email, password, walletAddress });
             if (response.status === 200) {
                 console.log('Login successful');
-                // Redirect or handle the login success scenario
             } else {
                 console.log(`Login failed with status: ${response.status}`);
             }
@@ -26,48 +26,50 @@ const Login = () => {
     return (
         <div>
             <Navbar />
-            <form onSubmit={handleSubmit}>
-                <div>
-                    <label htmlFor="username">Username:</label>
-                    <input
-                        type="text"
-                        id="username"
-                        value={username}
-                        onChange={e => setUsername(e.target.value)}
-                        required
-                    />
-                </div>
-                <div>
-                    <label htmlFor="email">Email:</label>
-                    <input
-                        type="email"
-                        id="email"
-                        value={email}
-                        onChange={e => setEmail(e.target.value)}
-                        required
-                    />
-                </div>
-                <div>
-                    <label htmlFor="password">Password:</label>
-                    <input
-                        type="password"
-                        id="password"
-                        value={password}
-                        onChange={e => setPassword(e.target.value)}
-                        required
-                    />
-                </div>
-                <div>
-                    <label htmlFor="walletAddress">Wallet Address:</label>
-                    <input
-                        type="text"
-                        id="walletAddress"
-                        value={walletAddress}
-                        onChange={e => setWalletAddress(e.target.value)}
-                    />
-                </div>
-                <button type="submit">Login</button>
-            </form>
+            <div className="form-container">
+                <form onSubmit={handleSubmit}>
+                    <div className="field-container">
+                        <label htmlFor="username">Username:</label>
+                        <input
+                            type="text"
+                            id="username"
+                            value={username}
+                            onChange={e => setUsername(e.target.value)}
+                            required
+                        />
+                    </div>
+                    <div className="field-container">
+                        <label htmlFor="email">Email:</label>
+                        <input
+                            type="email"
+                            id="email"
+                            value={email}
+                            onChange={e => setEmail(e.target.value)}
+                            required
+                        />
+                    </div>
+                    <div className="field-container">
+                        <label htmlFor="password">Password:</label>
+                        <input
+                            type="password"
+                            id="password"
+                            value={password}
+                            onChange={e => setPassword(e.target.value)}
+                            required
+                        />
+                    </div>
+                    <div className="field-container">
+                        <label htmlFor="walletAddress">Wallet Address:</label>
+                        <input
+                            type="text"
+                            id="walletAddress"
+                            value={walletAddress}
+                            onChange={e => setWalletAddress(e.target.value)}
+                        />
+                    </div>
+                    <button type="submit">Login</button>
+                </form>
+            </div>
         </div>
     );
 };
