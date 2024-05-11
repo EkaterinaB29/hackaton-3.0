7bfa734884b35a5eb2d2f62e754f7840c19c6c2d
krisss <kpiyarska@gmail.com>
2024-05-12 00:23:56 +0200

    Update frontend

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const jwt = require('jsonwebtoken');
 
 // POST endpoint for /api
 router.get('/userinfo', (req, res) => {
@@ -18,12 +19,22 @@ router.post('/login', (req, res) => {
     const db = req.db;  // Access the db connection
     // Use the db connection for your database queries
     db.query('SELECT * FROM users WHERE username = ? AND password = ?', [username, password], (err, results) => {
+
+
         if (err) throw err;
         if (results.length > 0) {
-            res.send('Login successful');
+            const token = jwt.sign({ id: results[0].id }, 'your_secret_key', { expiresIn: '1h' });
+            res.json({
+                message: 'Login successful',
+                token: token,
+                loggedin: true
+            });
             console.log('Login successful');
         } else {
-            res.send('Login failed');
+            res.json({
+                message: 'Login failed',
+                loggedin: false
+            });
             console.log('Login failed');
         }
     });
