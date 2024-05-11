24a636a7a5e56235c157845862b3874a8c60ab2b
Nenad Jakovchevski <89221061@student.upr.si>
2024-05-11 21:04:04 +0200

    DELETION OF WALLETDB

--- backend/services/paymentProcessor.js
+++ backend/services/paymentProcessor.js
@@ -1,13 +1,11 @@
 class PaymentProcessor {
-    constructor(blockchainInterface, exchangeService, walletDB) {
+    constructor(blockchainInterface, exchangeService) {
         this.blockchainInterface = blockchainInterface;
         this.exchangeService = exchangeService;
-        this.walletDB = walletDB; // Database access layer for storing/retrieving wallet addresses
     }
 
     async processPayment(fromAddress, toAddress, amount, crypto, fiat) {
         try {
-            const senderWallet = this.walletDB.getWallet(fromAddress); // Retrieve sender's wallet details from DB
             const transaction = senderWallet.createTransaction(toAddress, amount);
             const txResult = await this.blockchainInterface.sendTransaction(transaction);
             const convertedAmount = await this.exchangeService.convertToCurrency(amount, crypto, fiat);
