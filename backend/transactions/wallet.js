fa5ead5287c5f40d0fb105cfa7e468d346ff5e15
Nenad Jakovchevski <89221061@student.upr.si>
2024-05-11 17:14:16 +0200

    Added axios for development on packages.

---
+++ backend/transactions/wallet.js
@@ -0,0 +1,19 @@
+const { ec: EC } = require('elliptic');
+const { Transaction } = require('./transactions/Transaction');
+const ec = new EC('secp256k1');
+
+class Wallet {
+    constructor() {
+        const keyPair = ec.genKeyPair();
+        this.privateKey = keyPair.getPrivate('hex');
+        this.publicKey = keyPair.getPublic('hex');
+    }
+
+    createTransaction(toAddress, amount) {
+        const transaction = new Transaction(this.publicKey, toAddress, amount);
+        transaction.signTransaction(ec.keyFromPrivate(this.privateKey));
+        return transaction;
+    }
+}
+
+module.exports = Wallet;
