e3b8fc4ac40f66d77e9cac1c46d681e1c9ee12da
krisss <kpiyarska@gmail.com>
2024-05-11 18:15:28 +0200

    Update frontend

--- frontend/src/Components/Item.js
+++ frontend/src/Components/Item.js
@@ -1,12 +1,11 @@
 import React from 'react';
-    
 
 const Item = ({ name, price, imagePath, brand, font }) => {
     return (
         <div className='items'>
+            <img src={imagePath} alt={name} />
             <p>Sponsored</p>
             <p>{font}</p>
-            <img src={imagePath} alt={name} />
             <p>{price}€</p>
         </div>
     );
