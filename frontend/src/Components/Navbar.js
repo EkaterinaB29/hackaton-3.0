b512c8d895b70fd59cde4781aafcdf410bbadbc7
krisss <kpiyarska@gmail.com>
2024-05-11 19:50:02 +0200

    Update frontend

--- frontend/src/Components/Navbar.js
+++ frontend/src/Components/Navbar.js
@@ -4,12 +4,19 @@ import './css/navbar.css';
 const Navbar = () => {
     return (
         <nav className="navbar-container">
-            <div className="navbar-left">
-             
-                <a href="/">Women</a>
-                <a href="/">Men</a>
-                <a href="/">Children</a>
+            <div className="navbar-center">
+                <div className="navbar-left">
+                    <a href="/">Women</a>
+                    <a href="/">Men</a>
+                    <a href="/">Children</a>
+                </div>
+                <div className="navbar-right">
+                    <input type="search" placeholder="Search" className="search-input" />
+                    <a href="/" className="icon-link"><img src="https://cdn-icons-png.flaticon.com/512/4308/4308439.png" alt="Heart Icon" className='icon' /></a>
+                    <a href="/" className="icon-link"><img src="https://cdn-icons-png.flaticon.com/512/2956/2956820.png" alt="User Icon" className='icon' /></a>
+                </div>
             </div>
+        
             <div className="navbar-center">
                 <a href="/">News</a>
                 <a href="/">Clothes</a>
@@ -21,13 +28,11 @@ const Navbar = () => {
                 <a href="/">Premium</a>
                 <a href="/">Lowering</a>
             </div>
-            <div className="navbar-right">
-                <input type="search" placeholder="Search" className="search-input" />
 
-                <a href="/" className="icon-link"><img src="https://cdn-icons-png.flaticon.com/512/4308/4308439.png" alt="Heart Icon" className='icon' /></a>
-                <a href="/" className="icon-link"><img src="https://cdn-icons-png.flaticon.com/512/2956/2956820.png" alt="User Icon" className='icon' /></a>
-            </div>
+         
+
         </nav>
+    
     );
 };
 
